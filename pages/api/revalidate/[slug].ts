44b117198553cb7671d288b5a095158fc2bc8e16
Khalil El Ghoul <khalilelghoul01@gmail.com>
2022-07-13 19:29:49 +0200

    update

--- pages/api/revalidate/[slug].ts
+++ pages/api/revalidate/[slug].ts
@@ -1,12 +1,13 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { getAllChapters } from '../../../db/chapters'
+import { server } from './../../../config/index'
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
   const { slug } = req.query
-  await res.revalidate(`/chapter/${slug}`)
+  await res.revalidate(server + `/chapter/${slug}`)
   res.status(200).json({
     slug,
   })
