628a55bcbea93f602f75e11ab770f1693ea3231b
Khalil El Ghoul <khalilelghoul01@gmail.com>
2022-07-13 17:39:42 +0200

    update

---
+++ pages/api/revalidate_one.ts
@@ -0,0 +1,13 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { getAllChapters } from '../../db/chapters'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const { slug } = req.query
+  await res.revalidate(`/chapter/${slug}`)
+  res.status(200).json({
+    slug,
+  })
+}
